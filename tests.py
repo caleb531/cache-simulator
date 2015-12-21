4db9a6538052eb710bded7643af96bc4f31af1d1
Caleb Evans <caleb@calebevans.me>
2015-12-21 12:53:27 -0800

    Test is_hit function

--- tests.py
+++ tests.py
@@ -107,3 +107,27 @@ def test_get_consecutive_words_4_word():
     nose.assert_list_equal(
         sim.get_consecutive_words(21, num_words_per_block=4),
         [20, 21, 22, 23])
+
+
+class TestCacheHitMiss(object):
+    "is_hit should behave correctly in all cases"
+
+    def __init__(self):
+        self.cache = {
+            '010': [{
+                'tag': '1011',
+                'data': [180, 181]
+            }]
+        }
+
+    def test_is_hit_true(self):
+        """is_hit should return True if index and tag exist in cache"""
+        nose.assert_true(sim.is_hit(self.cache, '010', '1011'))
+
+    def test_is_hit_false_index_mismatch(self):
+        """is_hit should return False if index does not exist in cache"""
+        nose.assert_false(sim.is_hit(self.cache, '011', '1011'))
+
+    def test_is_hit_false_tag_mismatch(self):
+        """is_hit should return False if tag does not exist in cache"""
+        nose.assert_false(sim.is_hit(self.cache, '010', '1010'))
