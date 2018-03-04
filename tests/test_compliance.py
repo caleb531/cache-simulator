3bf750f8569b4ddfa6f0b208249dcbe0edde952d
Caleb Evans <caleb@calebevans.me>
2018-03-03 19:24:24 -0800

    Upgrade all dependencies

--- tests/test_compliance.py
+++ tests/test_compliance.py
@@ -2,14 +2,14 @@
 
 import glob
 import nose.tools as nose
-import pep8
+import pycodestyle
 import radon.complexity as radon
 
 
 def test_pep8():
     file_paths = glob.iglob('*/*.py')
     for file_path in file_paths:
-        style_guide = pep8.StyleGuide(quiet=True)
+        style_guide = pycodestyle.StyleGuide(quiet=True)
         total_errors = style_guide.input_file(file_path)
         test_pep8.__doc__ = '{} should comply with PEP 8'.format(file_path)
         fail_msg = '{} does not comply with PEP 8'.format(file_path)
