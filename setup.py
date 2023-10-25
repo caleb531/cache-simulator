965cf71bd274b7062da82d28069db265e265e5ef
Caleb Evans <caleb@calebevans.me>
2023-10-25 16:16:41 -0700

    Move package metadata to pyproject.toml

--- setup.py
+++ setup.py
@@ -3,29 +3,4 @@
 
 from setuptools import setup
 
-
-# Get long description (used on PyPI project page)
-def get_long_description():
-    with open('README.md', 'r') as readme_file:
-        return readme_file.read()
-
-
-setup(
-    name='cache-simulator',
-    version='2.0.1',
-    description='A processor cache simulator for the MIPS ISA',
-    long_description=get_long_description(),
-    long_description_content_type='text/markdown',
-    url='https://github.com/caleb531/cache-simulator',
-    author='Caleb Evans',
-    author_email='caleb@calebevans.me',
-    license='MIT',
-    keywords='mips processor cache simulator architecture',
-    packages=['cachesimulator'],
-    install_requires=[],
-    entry_points={
-        'console_scripts': [
-            'cache-simulator=cachesimulator.__main__:main'
-        ]
-    }
-)
+setup()
